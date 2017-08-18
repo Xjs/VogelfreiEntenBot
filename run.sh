de3e0f7f633e4f6af66ea41e1d465530522e4911
Marco Schlicht <marcoschlicht@onlinehome.de>
2017-08-18 20:58:22 +0200

    run script angepasst

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-BASEDIR=$(dirname "$0")
-echo "$BASEDIR"
-cd $BASEDIR
-java -cp bin:lib/telegrambots-2.4.4.5-jar-with-dependencies.jar Main
+BASEDIR=$(dirname "$0");
+cd $BASEDIR;
+java -cp bin:lib/telegrambots-2.4.4.5-jar-with-dependencies.jar Main 2> run.error.log >/dev/null &
+PID=$!;
+echo "$PID";

3ed705be5270f84914bb8f470243348e81186819
LenVol <l.volz@stud.uni-heidelberg.de>
2018-05-16 18:22:31 +0200

    Some idea I had

--- src/EntenBot.java
+++ src/EntenBot.java
@@ -91,6 +91,12 @@ public class EntenBot extends TelegramLongPollingBot {
 				}
 			} else if (text.contains("dachsenfranz")) {
 				message.setText("*Prosit! 🍻*");
+			} else if (text.contains("spät") '' text.contains("spaet")){
+				if (Math.random)< 0.2) {
+					message.setText("Das geht ja mal gar nicht! Du bist schon wieder spät dran? Überdenke lieber einmal deine Lebensentscheidungen. Und dann auch noch über Telegram schreiben. Schäm dich!")
+				} else {
+					message,SetText("War ja zu erwarten das gerade DU mal wieder spät dran bist...")
+				}
 			} else {
 				return;
 			}
